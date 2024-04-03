547a1b560779432c8e58906feb68739d73d7ec50
NazrinAli1 <anazrin711@gmail.com>
2024-04-03 03:12:03 -0600

    accesd-mod.-encaps.-task-added

---
+++ Access Modifiers, Encapsulation-Task/Methods/Weapon.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Access_Modifiers__Encapsulation_Task.Methods
+{
+    public class Weapon
+    {
+        int _bulletCapacity;
+        int _bulletCount;
+        float _fireRate;
+        bool _isAutomaticMode;
+        int _resetTimeInSecond;
+
+        public int BulletCapacity
+        {
+            get { return _bulletCapacity;}
+            set { _bulletCapacity = value;}
+        }
+        public int BulletCount
+        {
+            get { return _bulletCount; }
+            set { _bulletCount = value; }
+        }
+        public float FireRate
+        {
+            get { return _fireRate; }
+            set { _fireRate = value; }
+        }
+        public bool IsAutomaticMode
+        {
+            get { return _isAutomaticMode; }
+            set { _isAutomaticMode = value; }
+        }
+        public int ResetTimeSeconds
+        {
+            get { return _resetTimeInSecond; }
+            set { _resetTimeInSecond = value; }
+        }
+            public Weapon(int bulletcapac, int bulletcount, float firerate, bool isAutomaticMode)
+        {
+            _bulletCapacity = bulletcapac;
+            _bulletCount= bulletcount;
+            _fireRate= firerate;
+            _isAutomaticMode = isAutomaticMode;
+
+
+        }
+
+        public void Fire()
+        {
+            if (_bulletCount == 0)
+            {
+                Console.WriteLine("Daraqda gulle yoxdur. Yeniden doldurun.");
+                return;
+            }
+            int buletsFired = 0;
+            DateTime startTime = DateTime.Now;
+
+            while (_bulletCount>0)
+            {
+                Shoot();
+                buletsFired++;
+                if (!_isAutomaticMode)
+                {
+                    break;//Tək atış rejimində bir dəfə atəş etdikdən sonra döngədən çıxın
+                }
+            }
+            DateTime endTime = DateTime.Now;
+            TimeSpan totalTime = endTime - startTime;
+            float fireRate = buletsFired / (float)totalTime.TotalSeconds;
+
+            Console.WriteLine($"Gullelerin hamisi ateslendi. Bitme deqiqesi: {totalTime}. Atis derecesi: {fireRate} gulle/saniye. (Atis derecesi, 1 saniyede atilan gulle sayisini ifade edir.)");
+
+        }
+        public void GetSet()
+        {
+            Random random = new Random();
+            byte usedBullet = Convert.ToByte(random.Next(_bulletCount));
+            _bulletCount -= usedBullet;
+            if (_bulletCount < 0)
+            {
+                _bulletCount = 0;
+            }
+            Console.WriteLine($"Used bullet {usedBullet}. Bulletcount : {_bulletCount}");
+        }
+        public void Shoot()
+        {
+            if (_bulletCount > 0)
+            {
+                Console.WriteLine("Bir gulle atildi.");
+                _bulletCount--;
+            }
+            else
+            {
+                Console.WriteLine("Daraqda gule qalmadi. Yeniden doldurun.");
+
+            }
+
+        }
+        public int GetRemainBulletCount()
+        {
+            return _bulletCapacity - _bulletCount;
+
+        }
+        public void Reload()
+        {
+            //daragi yeniden doldurur.
+            //_bulletCount = _bulletCapacity;
+            int bulletsAdd = _bulletCapacity - _bulletCount;
+            _bulletCount += bulletsAdd;
+            Console.WriteLine($"Daraq yeniden dolduruldu. Elave olunan mermi sayi: {bulletsAdd}. Yeni mermi sayi: {_bulletCount}");
+        }
+        public void ChangeFireMode()
+        {
+            if (_isAutomaticMode)
+            {
+                _isAutomaticMode = false;
+                Console.WriteLine("Atis modu deyisdirildi: Tekli");
+            }
+            else
+            {
+                _isAutomaticMode = true;
+                Console.WriteLine("Atis modu deyisdirildi: Avtomatik");
+            }
+        }
+    }
+
+}
---
+++ Access Modifiers, Encapsulation-Task/Program.cs
@@ -0,0 +1,87 @@
+﻿using Access_Modifiers__Encapsulation_Task.Methods;
+
+namespace Access_Modifiers__Encapsulation_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isContinue = true;
+            Weapon weapon = new Weapon(bulletcapac : 40, bulletcount: 14, firerate: 0.5f, isAutomaticMode : true);
+            do
+            {
+
+                Console.WriteLine(@"Bütün məlumatları doldurmadan obyekt yaratmaq olmasın.
+0 - İnformasiya almaq üçün
+1 - Shoot metodu üçün
+2 - Fire metodu üçün
+3 - GetRemainBulletCount metodu üçün
+4 - Reload metodu üçün
+5 - ChangeFireMode metodu üçün
+6 - Proqramdan dayandırmaq üçün
+qısayoldur.
+7 - Redaktə et :
+T - Tutumu dəyişsin
+S - Güllə sayı
+D - Sıfırlama saniyəsi");
+
+                int input = Convert.ToInt32(Console.ReadLine());
+                switch (input)
+                {
+                    case 0:
+                        weapon.GetSet();
+                        break;
+                    case 1:
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        break;
+                    case 3:
+                        int remainBulletCount = weapon.GetRemainBulletCount();
+                        Console.WriteLine($"Qalan gulle sayi: {remainBulletCount}");
+                        break;
+                    case 4:
+                        weapon.Reload();
+                        break;
+                    case 5:
+                        weapon.ChangeFireMode();
+                        break;
+                    case 6:
+                        isContinue = false;
+                        break;
+                        Console.Clear();
+
+                    case 7:
+                        Console.WriteLine("Redakte et:");
+                        string editInput = Console.ReadLine();
+                        switch (editInput.ToUpper())
+                        {
+                            case "T":
+                                Console.WriteLine("Tutum deyisdirilsin: ");
+                                int newBuletCapacity= Convert.ToInt32(Console.ReadLine());
+                                weapon.BulletCapacity = newBuletCapacity;
+                                break;
+                            case "S":
+                                Console.WriteLine("Gulle sayi deyisdirilsin: ");
+                                int newBulletCount = Convert.ToInt32(Console.ReadLine());
+                                weapon.BulletCount = newBulletCount;
+                                break;
+                            case "D":
+                                Console.WriteLine("Sifirlama saniyesi deyissin.");
+                                int newTesetTimeSeconds = Convert.ToInt32(Console.ReadLine());
+                                weapon.ResetTimeSeconds = newTesetTimeSeconds;
+                                break;
+                            default:
+                                Console.WriteLine("Duzgub bir secim etmediniz.");
+                                break;
+                        }
+                        break;
+
+                }
+            }while(isContinue);
+
+
+        }
+    }
+}
